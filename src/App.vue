7950c08573718edd1b7a6fcf2004471e33ef3204
Rickard <102632224+locorickardo@users.noreply.github.com>
2025-02-17 11:58:38 +0100

    Added herosection to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterView } from VueRouter
 import NavBar from '../src/components/NavBar.vue'
 import Footer from '../src/components/Footer.vue'
+import HeroSection from '../src/components/HeroSection.vue'
 
 
 </script>
 
 <template>
     <NavBar></NavBar>
+    <HeroSection></HeroSection>
     <RouterView></RouterView>
     <Footer></Footer>
 </template>
