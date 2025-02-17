f9eaf81c1447d191a947bd861f1a5386c5c50e13
Rickard <102632224+locorickardo@users.noreply.github.com>
2025-02-17 11:54:10 +0100

    Added RouterView and basic components to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { RouterView } from VueRouter
+import NavBar from '../src/components/NavBar.vue'
+import Footer from '../src/components/Footer.vue'
+
 
 </script>
 
 <template>
-
+    <NavBar></NavBar>
+    <RouterView></RouterView>
+    <Footer></Footer>
 </template>
 
 <style scoped></style>
