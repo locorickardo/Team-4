134ed4ea81d599e13fb7ba1680fa6a2bc374a08a
Abualtaher <wisamaltaher@gmail.com>
2025-02-18 13:38:33 +0100

    #4 jag fixad app men inte saker om bootstrap men jag kommer och fixa

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
+<script setup></script>
 <template>
-
+  <div>About</div>
 </template>
-
-<style></style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,5 @@
-<script setup>
-
-</script>
-
+<script setup></script>
 <template>
-
+  <div>content</div>
 </template>
+<style scoped></style>
