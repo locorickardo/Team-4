e95dc974f27325e9f66f4ee9f73edd4c9db8018e
Rickard <102632224+locorickardo@users.noreply.github.com>
2025-02-19 09:33:35 +0100

    Fixed Bootstrap errors, added Navbar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+/* Bootstrap CSS */
+import 'bootstrap/dist/css/bootstrap.css'
+/* Bootstrap JavaScript (e.g. modal, tooltips, etc)
+Den här behövs för att JavaScript ska fungera i Bootstrap */
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
