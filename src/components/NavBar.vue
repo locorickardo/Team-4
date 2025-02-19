1fb48da8530c38bddacfb94937c3f31909706ac3
Abualtaher <wisamaltaher@gmail.com>
2025-02-19 21:28:34 +0100

    Fixade links och form #4

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,7 +14,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
           <li class="nav-item">
             <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
           </li>
@@ -22,6 +22,10 @@
             <RouterLink class="nav-link" to="/about">About</RouterLink>
           </li>
         </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </nav>
