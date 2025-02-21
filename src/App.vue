0718bf60c449b8cb2e0ab4c7058ff359b1c2e218
Abualtaher <wisamaltaher@gmail.com>
2025-02-22 00:24:52 +0100

    resolving comflict

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script setup>
 import NavBar from '../src/components/NavBar.vue'
-<<<<<<< HEAD
+
 import FooterLast from './components/FooterLast.vue'
-=======
+
 import HeroSection from '../src/components/HeroSection.vue'
->>>>>>> origin/main
 </script>
 
 <template>
   <NavBar />
-<<<<<<< HEAD
 
   <RouterView />
   <FooterLast />
-=======
+
   <HeroSection />
   <RouterView />
->>>>>>> origin/main
 </template>
 
 <style scoped></style>
