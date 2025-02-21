d023be216038583ce630414db201740000d2440b
Abualtaher <wisamaltaher@gmail.com>
2025-02-22 00:30:11 +0100

    w

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import NavBar from '../src/components/NavBar.vue'
-
 import FooterLast from './components/FooterLast.vue'
-
 import HeroSection from '../src/components/HeroSection.vue'
 </script>
 
