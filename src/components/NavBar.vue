f07985ab611d0fc0c42a5e540f2c4b2bb4b6205e
Abualtaher <wisamaltaher@gmail.com>
2025-02-25 12:06:05 +0100

    skapade logo och det finns i app #32

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Taste Tales</a>
+      <a class="navbar-brand" href="#"
+        ><img class="logo" src="/src/assets/imge/Tasty Tales.png" alt=""
+      /></a>
       <button
         class="navbar-toggler"
         type="button"
