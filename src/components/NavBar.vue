0d753bf2e9a8c0bc796f2fd6c797cb5ad9d26cf6
Abualtaher <wisamaltaher@gmail.com>
2025-02-25 12:15:32 +0100

    added link to the logo #32

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"
+      <a class="navbar-brand" href="/"
         ><img class="logo" src="/src/assets/imge/Tasty Tales.png" alt=""
       /></a>
       <button
