f9fe3bc316ccc78e6ba2a81240607591266859bc
Rickard <102632224+locorickardo@users.noreply.github.com>
2025-03-07 12:04:25 +0100

    fixed navbar staying open even after clicking a link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top p-0">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/"
-        ><img class="logo" src="/src/assets/img/Tasty Tales.png" alt=""
-      /></a>
+      <a class="navbar-brand" href="/">
+        <img class="logo" src="/src/assets/img/Tasty Tales.png" alt="" />
+      </a>
       <button
         class="navbar-toggler"
         type="button"
@@ -18,22 +18,44 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
           <li class="nav-item">
-            <RouterLink class="nav-link" aria-current="page" to="/">Home</RouterLink>
+            <RouterLink class="nav-link" to="/" @click="closeNavbar">Home</RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="nav-link" to="/recept">Recept</RouterLink>
+            <RouterLink class="nav-link" to="/recept" @click="closeNavbar">Recept</RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="nav-link" to="/favoriter">Mina Favoriter</RouterLink>
+            <RouterLink class="nav-link" to="/favoriter" @click="closeNavbar">Mina Favoriter</RouterLink>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Ensure Bootstrap JS is available
+
+const route = useRoute()
+
+function closeNavbar() {
+  const navbarCollapse = document.querySelector('.navbar-collapse')
+  if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+    navbarCollapse.classList.remove('show') // This closes the menu
+  }
+}
+
+// Close navbar when clicking a link
+onMounted(() => {
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', closeNavbar)
+  })
+})
+</script>
+
 <style>
 .navbar {
   background-color: #f4f0e8 !important;
 }
 </style>
-<script setup></script>
